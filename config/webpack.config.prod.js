ba9bfdba0543636f007985d1dcfa896af6036cff
mgechev <mgechev@gmail.com>
2018-03-12 18:18:28 -0700

    chore: add data file

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -14,6 +14,7 @@ const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { MLPlugin } = require('@mlx/webpack');
+const data = require('../data.json');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -326,7 +327,7 @@ module.exports = {
       tsconfig: paths.appTsConfig,
       tslint: paths.appTsLint
     }),
-    new MLPlugin({ data: require('../../mlx-ga-demo/data.json') })
+    new MLPlugin({ data })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
